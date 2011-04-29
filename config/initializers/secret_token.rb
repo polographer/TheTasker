1087e2f4aea6ea609f37ccab50a3f3ba4c4506f1
Polo Ornelas <warstories@shogoki.(none)>
2011-04-29 11:50:42 -0500

    Initial rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheTasker::Application.config.secret_token = '0fb67b78e6fed04da3a8e60ef87b65d2a1c882aeaffc194804df7a095213db61310264a815725a43247760f629fc12ff93b405ea8a77618bffa78f1e4c28cb02'
